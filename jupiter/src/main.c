f7292761f3c75096fd2ec2d44397f1b41745c8af
lshi@espial.com <lshi@espial.com>
2017-11-10 14:49:34 -0500

    Init and package infor.. HAVE_CONFIG_H still not working

--- jupiter/src/main.c
+++ jupiter/src/main.c
@@ -1,9 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+//Autoconf-provided portabiliyt
+
+#if HAVE_DLFCN_N
+#include <dlfcn.h>
+#endif
+
+
+
 
 int main(int argc ,char * argv[])
 {
+void *handle;
 	printf("Hello from %s\n", argv[0]);
-
+#if HAVE_DLFCN_H
+	handle= dlopen("/usr/lib/libblas.so.3", RTLD_NOW);
+	printf("dlopen %p\n", handle);
+#endif
 	return 0;
 }
