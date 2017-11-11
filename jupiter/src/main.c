cb16899020c791ae752c56ca5a02c415c6a911b1
linuxemb <itss07@gmail.com>
2017-11-10 22:44:09 -0500

    CH4

--- jupiter/src/main.c
+++ jupiter/src/main.c
@@ -1,20 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
+
 #if HAVE_CONFIG_H
 #include <config.h>
 #endif
 //Autoconf-provided portabiliyt
 
-#if HAVE_DLFCN_N
+#if HAVE_DLFCN_H
 #include <dlfcn.h>
 #endif
 
-
+static void * print_it(void* data)
+{
+	printf("Hello from thread %s!\n", (const char *) data);
+	return 0;
+}
 
 
 int main(int argc ,char * argv[])
 {
-void *handle;
+	pthread_t tid;
+	pthread_create(&tid, 0, print_it, argv[0]);
+	pthread_join(tid,0);
+	void *handle;
 	printf("Hello from %s\n", argv[0]);
 #if HAVE_DLFCN_H
 	handle= dlopen("/usr/lib/libblas.so.3", RTLD_NOW);
