b92a24653f26d9d45e2590432b67d400e39e42e9
linuxemb <itss07@gmail.com>
2017-11-11 20:48:57 -0500

    Coding Up with Feature Option

--- jupiter/config.h
+++ jupiter/config.h
@@ -1,6 +1,9 @@
 /* config.h.  Generated from config.h.in by configure.  */
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
+/* async execution enabled */
+#define ASYNC_EXEC 1
+
 /* Define to 1 if you have the <dlfcn.h> header file. */
 #define HAVE_DLFCN_H 1
 
