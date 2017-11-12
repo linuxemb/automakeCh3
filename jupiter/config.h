fda479f901e8476348a8f14e390eee06595df365
linuxemb <itss07@gmail.com>
2017-11-11 20:18:58 -0500

    fix bug, now if test works

--- jupiter/config.h
+++ jupiter/config.h
@@ -11,7 +11,7 @@
 #define HAVE_MEMORY_H 1
 
 /* Define to 1 if you have the <pthread.h> header file. */
-/* #undef HAVE_PTHREAD_H */
+#define HAVE_PTHREAD_H 1
 
 /* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
